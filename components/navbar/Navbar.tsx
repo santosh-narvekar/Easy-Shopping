2f14f9aecf56a30cb3a56c9aa5015e0fcd280011
git-user <Admin@Desktop-U2F8MKR>
2024-08-13 18:00:58 +0530

    final commit

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -13,10 +13,12 @@ async function Navbar(){
   const getItems = await getItemsInCart(user?.id || null);
   //console.log('get',getItems)
   
-  return <nav className='navbar z-40 dark:bg-primary-foreground border-b-[1px] max-w-full px-4 md:px-8 py-4 md:py-8  flex items-center flex-wrap md:flex-nowrap justify-between '>
+  return <nav className='navbar z-40 dark:bg-primary-foreground border-b-[1px] max-w-full px-4 md:px-8 py-2 md:py-8  flex items-center flex-wrap md:flex-nowrap justify-between  '>
       <div className='flex items-center gap-2 md:gap-0 w-full'>
       <Logo />
+      <div className='self-center'>
       <SearchInput />
+      </div>
       </div>
       <div className='flex items-center gap-4 md:my-0 my-3'>
         <Cart Items={getItems} />
