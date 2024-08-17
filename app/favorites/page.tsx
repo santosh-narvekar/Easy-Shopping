a269f14986ae1758de635d60d78dda3daff03fd1
Santosh Narvekar <120112762+santosh-narvekar@users.noreply.github.com>
2024-08-17 19:27:42 +0530

    Update page.tsx

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -11,8 +11,8 @@ async function FavoritesPage(){
   
   return (
     <>
-    <h3 className=" text-3xl md:text-4xl capitalize font-bold mx-8">My Favorites</h3>
-    <section className="mx-8 grid md:grid-cols-2 lg:grid-cols-3 lg:gap-8 gap-4 md:gap-4 my-4 border-t-[1px] py-4 border-blue-100">
+    <h3 className=" text-3xl md:text-4xl capitalize font-bold mx-4 md:mx-8">My Favorites</h3>
+    <section className="md:mx-8 mx-4 grid md:grid-cols-2 lg:grid-cols-3 lg:gap-8 gap-4 md:gap-4 my-4 border-t-[1px] py-4 border-blue-100">
       {
         favorites.length === 0 && <EmptyList heading='No Favorites' message = "Add Your Favorites here" />
       }
